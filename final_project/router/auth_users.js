5f5b3a16ef9bf5c82984f4d4790cd32b5c30664b
sam-Thomas314 <Samuelajt@gmail.com>
2024-12-05 11:00:48 -0500

    The register workd, but the login does not work with my current implementation of authorizeduser

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,25 +6,29 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-    if(users[username]) {
-        return true;
-    } else {
-        return false
-    }
+    let userwithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    return userwithsamename.length > 0;
 //write code to check is the username is valid
-}
+};
 
 const authenticatedUser = (username,password)=>{
     let valid_users = users.filter((user) => {
-        return user.username === username && user.password === password;
+        if(users[user].username === username && users[username].password === password) {
+            valid_users.push({"username": username,
+            "password": password})
+        } 
     });
-    return valid_users.length > 0;
-
-    
+    if(valid_users.length > 0) {
+        return true;
+    } else {
+        return false;
+    };
     
 //returns boolean
 //write code to check if username and password match the one we have in records.
-}
+};
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
@@ -36,16 +40,18 @@ regd_users.post("/login", (req,res) => {
     return res.status(404).json({message: "Please ented a username and password"})
   } 
 
-  if(authenticatedUser(username, password)) {
+  if(username && password) {
     let accessToken = jwt.sign({
         data: password
     }, 'access', {expiresIn: 70 * 70});
     
-    res.session.authorization = {
+    req.session.authorization = {
         accessToken, username
     };
+        return res.status(200).json({message: "User successfully logged in"});
+
     } else {
-        return res.status(208).json({message: "Invalid Login. Check username and Password"});
+        return res.status(403).json({ message: "User not logged in" });
     }
   //return res.status(300).json({message: "Yet to be implemented"});
 });
@@ -53,6 +59,22 @@ regd_users.post("/login", (req,res) => {
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
+
+  const isbn = req.params.isbn;
+  const review = req.params.review;
+  const username = req.params.username;
+
+  let filtered_books = books.filter((book) => book.isbn === isbn);
+
+  if(filtered_books.length >0) {
+    let one_book = filtered_books[0];
+
+    if(review) {
+        one_book.review = review;
+    }
+    
+  }
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
