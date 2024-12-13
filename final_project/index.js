20435a796e69ecb381e266bc940134fc7fdc7327
Sam-Thomas314 <Samuelajt@gmail.com>
2024-12-13 09:57:06 -0500

    Register and add review function added

--- final_project/index.js
+++ final_project/index.js
@@ -12,22 +12,21 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
-if(req.session.authenticated) {
-    let token = req.session.authenticated['accessToken'];
+    let token = req.session.authorization;
+    if(token) {
+        token = token['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if(!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({message: "Customer not authenticated"})
 
-    jwt.verify(token, 'access', (err, user) => {
-        if(!err) {
-            req.user = user;
-            next(); //proceed to the next middleware
-        } else {
-            return res.status(403).json({message: "User not authenticated"});
-        }
-    });
-} else {
-    return res.status(403).json({message: "User not logged in"});
-}
-
-
+            }
+        });
+    } else {
+        return res.status(403).json({message: "Customer not logged in"})
+    }
 });
  
 const PORT =5000;
